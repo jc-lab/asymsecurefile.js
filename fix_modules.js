055480842401bed19fef9d9d821f66c8fa97fbab
jichan <joseph@jc-lab.net>
2022-02-08 18:00:05 +0900

    chore(deps): use yarn and update common-crypto

--- fix_modules.js
+++ fix_modules.js
@@ -1,21 +1,28 @@
 const path = require('path');
 const fs = require('fs');
+const child_process = require('child_process');
 
-try {
-    fs.rmdirSync(path.resolve('./node_modules/pkijs/node_modules'), {
-        recursive: true
-    });
-} catch(e) {
-    console.error(e);
+if (process.env['npm_command']) {
+  child_process.execSync('npm dedupe', {
+    stdio: 'inherit'
+  });
 }
 
 try {
-    const file = path.resolve(__dirname, './node_modules/@types/asn1js');
-    if (fs.existsSync(file)) {
-        fs.rmdirSync(file, {
-            recursive: true
-        });
-    }
-} catch(e) {
-    console.error(e);
+  fs.rmdirSync(path.resolve('./node_modules/pkijs/node_modules'), {
+    recursive: true
+  });
+} catch (e) {
+  console.error(e);
+}
+
+try {
+  const file = path.resolve(__dirname, './node_modules/@types/asn1js');
+  if (fs.existsSync(file)) {
+    fs.rmdirSync(file, {
+      recursive: true
+    });
+  }
+} catch (e) {
+  console.error(e);
 }
